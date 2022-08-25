2e97fa760c12b870f050cfd0dcaeae69f9eb6b76
Ananya Ohrie <86018166+ananyao3@users.noreply.github.com>
2022-08-25 19:39:29 -0400

    Create wordlist.c

---
+++ wordlist.c
@@ -0,0 +1,16 @@
+const int hangman_wordlist_len = 12;
+
+char *hangman_wordlist[12] = {
+  "cool",
+  "amazing",
+  "happy",
+  "beautiful",
+  "insane",
+  "summer",
+  "funny",
+  "makeup",
+  "grateful",
+  "show",
+  "interesting",
+  "nature"
+};
